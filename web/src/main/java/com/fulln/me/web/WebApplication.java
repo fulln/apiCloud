7b46072bf99718b17ca761d7dff524594f832c63
fulln <245193247@qq.com>
2019-09-18 00:38:38 +0800

    修改当前的config启动项为本地配置读取，去掉了web的exclut

--- web/src/main/java/com/fulln/me/web/WebApplication.java
+++ web/src/main/java/com/fulln/me/web/WebApplication.java
@@ -1,17 +1,16 @@
 package com.fulln.me.web;
 
-import com.fulln.me.api.common.Mongo.MongoHelper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 
 @EnableFeignClients
 @EnableEurekaClient
-@ComponentScan(value = {"com.fulln.me"},excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {MongoHelper.class}))
+@ComponentScan(value = {"com.fulln.me"})
+//,excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {MongoHelper.class}))
 @SpringBootApplication(exclude = MongoAutoConfiguration.class)
 public class WebApplication {
 
